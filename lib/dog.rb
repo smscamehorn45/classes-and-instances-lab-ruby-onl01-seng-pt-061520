b83929a3b9e90ff03c619e374903fe9e8783f547
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:06:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+
+class Dog 
+end 
+
+
+class Dog
+end
+
+fido = Dog.new
+fido 
+  
+
+
+class Dog
+end
+
+fido = Dog.new
+snoopy = Dog.new
+snoopy
+
+
+
+lassie = Dog.new
+lassie
+
+ 
+
+snoopy == fido 
+# => false
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+end 
+
+
+class Person  
+end
+
+adele_goldberg = Person.new
+adele_goldberg 
+  
+
+
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+alan_kay
+
