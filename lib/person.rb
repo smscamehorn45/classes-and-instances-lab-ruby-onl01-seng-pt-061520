391001e530fc5662703b53efa53b5f44747581e9
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-30 01:03:10 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+end 
+
+
+class Person
+end
+
+adele_goldberg = person.new
+adele_goldberg
+  
+
+
+class Person
+end
+
+ adele_goldberg= Person.new
+alan_kay = Person.new
+alan_kay
+
+
+
+=
